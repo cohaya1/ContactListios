aa0b50304e7d3c65a98a3472379291d77c6184af
cohaya1 <chika.ohaya@gmail.com>
2019-10-17 19:21:02 -0400

    Initial Commit

---
+++ MyContactList/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyContactList
+//
+//  Created by Makaveli Ohaya on 10/17/19.
+//  Copyright © 2019 Makaveli Ohaya. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MyContactList/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyContactList
+//
+//  Created by Makaveli Ohaya on 10/17/19.
+//  Copyright © 2019 Makaveli Ohaya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
